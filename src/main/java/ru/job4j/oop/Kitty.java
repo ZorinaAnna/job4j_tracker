4c0edd8fea025819d0cb4a5cf1491b2fbabc7f4c
Ann <annazorina@yandex.ru>
2025-01-23 23:36:32 +0500

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/oop/Kitty.java
+++ src/main/java/ru/job4j/oop/Kitty.java
@@ -2,18 +2,32 @@ package ru.job4j.oop;
 
 public class Kitty {
 
-    private String food; /* создали поле класса Cat, которое называется food */
+    private String food; /* Поле для хранения информации о еде */
+    private String name; /* Поле для хранения клички котика */
 
-    public void show() { /* метод будет выводить в консоль содержимое поля food */
-        System.out.println(this.food); /* чтобы обратиться к полю объекта, this, указывает нужно вывести содержимое поля */
+    public void giveNick(String nick) { /* Метод для выдачи клички котика */
+        this.name = nick; /* Записываем переданную кличку в поле name */
     }
 
-    public static void main(String[] args) {
-        System.out.println("There is gav's food.");
+    public void show() { /* Метод для показа информации о котике */
+        System.out.println(this.name + " ate " + this.food); /* Выводим кличку и информацию о еде */
+    }
+
+    public void eat(String meat) { /* Метод для кормления котика */
+        this.food = meat; /* Записываем в поле food значение переменной meat */
+    }
+
+    public static void main(String[] args) { /* Создаём первого котика */
+        System.out.println("There is Gav's food.");
         Kitty gav = new Kitty();
-        gav.show();
-        System.out.println("There is black's food.");
+        gav.giveNick("Gav"); /* Назначаем кличку котику */
+        gav.eat("cutlet"); /* Кормим котика */
+        gav.show(); /* Показываем информацию о котике */
+
+        System.out.println("There is Black's food."); /* Создаём второго котика */
         Kitty black = new Kitty();
-        black.show();
+        black.giveNick("Black"); /* Назначаем кличку котику */
+        black.eat("fish"); /* Кормим котика */
+        black.show(); /* Показываем информацию о котике */
     }
 }
