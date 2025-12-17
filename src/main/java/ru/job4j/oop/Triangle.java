6da667d75dd1ba71c87d9f624b5fe25abda081b0
Ann <annazorina@yandex.ru>
2025-12-17 21:16:26 +0500

    2.1. Рефакторинг - Площадь треугольника. [#122642 #556177]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -12,11 +12,11 @@ public class Triangle {
     }
 
     public double semiPerimeter(double a, double b, double c) {
-        return -1;
+        return (a + b + c) / 2;
     }
 
     public boolean exist(double ab, double ac, double bc) {
-        return false;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
 
     public double area() {
@@ -26,9 +26,11 @@ public class Triangle {
         double bc = second.distance(third);
         if (this.exist(ab, ac, bc)) {
             double p = semiPerimeter(ab, ac, bc);
-            double area = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
-            result = -1;
+            double s = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+            return s;
         }
-        return result;
+        return -1;
     }
 }
+
+
--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -8,11 +8,11 @@ public class TriangleTest {
     @Test
     public void when00and40and04Then8() {
         Point a = new Point(0, 0);
-        Point b = new Point(4, 0);
-        Point c = new Point(0, 4);
+        Point b = new Point(2, 0);
+        Point c = new Point(4, 0);
         Triangle triangle = new Triangle(a, b, c);
         double result = triangle.area();
-        double expected = 8;
+        double expected = -1;
         assertThat(result).isCloseTo(expected, offset(0.001));
     }
 }
